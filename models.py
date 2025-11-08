00df608abbde7ec8dd35aa825094ad8e69b04086
iooosef <josence22@gmail.com>
2025-11-08 12:50:33 +0800

    changed user model

--- models.py
+++ models.py
@@ -1,5 +1,15 @@
 from db import db
+from datetime import date
 
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    full_name = db.Column(db.String(120))
+    bday = db.Column(db.Date)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<User {self.username}>'
